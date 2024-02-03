1c2334aa54f605285153dec8a136a85718aa8b5b
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2024-02-02 17:03:44 -0700

    style the footer

--- mra/src/app/components/footer.js
+++ mra/src/app/components/footer.js
@@ -1,17 +1,22 @@
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
-
+import styles from '../styles/footer.module.css'
 
 export default function Footer() {
     return (
 
-        <>
+        <footer className={styles.footer}>
 
+        <div className={styles.gitHub}>
             <Link href="https://github.com/dannyvorton/my-real-age" target="_blank"><FontAwesomeIcon icon={faGithub} /></Link>
-            <Link href="https://www.linkedin.com/in/orton" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></Link>
+        </div>
 
-        </>
+        <div className={styles.linkedIn}>
+            <Link href="https://www.linkedin.com/in/orton" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></Link>
+        </div>
+
+        </footer>
 
     )
 }
