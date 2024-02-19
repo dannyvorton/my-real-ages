84694b28c5995e1117bbacebb371e6a7b47e87a0
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2024-02-19 10:48:29 -0700

    add angular

--- mraa/src/app/app.component.ts
+++ mraa/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ScriptureComponent } from "./scripture/scripture.component";
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, ScriptureComponent, HeaderComponent, FooterComponent]
 })
 export class AppComponent {
-  title = 'mraa';
+  title = 'My Real Age';
 }
---
+++ mraa/src/app/scripture/scripture.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scripture',
+  standalone: true,
+  imports: [],
+  templateUrl: './scripture.component.html',
+  styleUrl: './scripture.component.css'
+})
+export class ScriptureComponent {
+
+}
