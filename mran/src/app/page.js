f1691637f8fc3094df92810015f19251d2f5c3a7
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2024-02-25 20:18:45 -0700

    add header and scripture components

--- mran/src/app/page.js
+++ mran/src/app/page.js
@@ -1,11 +1,19 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Footer from "./components/footer"
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Scripture from "./components/scripture";
 
 export default function Home() {
   return (
 
-    <Footer/>
+    <fragment>
+
+      <Header/>
+      <Scripture/>
+      <Footer/>
+
+    </fragment>
     
   );
 }
