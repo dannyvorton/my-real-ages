0aa283a1383f00af8a6c0c4a89d1d07617de892a
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2024-03-02 15:40:15 -0700

    add components

---
+++ app/components/scripture.tsx
@@ -0,0 +1,33 @@
+export default function Scripture() {
+    return (
+
+        <div>
+
+            <blockquote>
+                For a thousand years in thy sight are but as yesterday when <br/>
+                it is past, and as a watch in the night. <br/>
+                Psalm 90:4
+            </blockquote>
+
+            <blockquote>
+                But, beloved, be not ignorant of this thing, that one day <br/>
+                is with the Lord as a thousand years, and a thousand years as <br/>
+                one day.
+                2 Peter 3:8
+            </blockquote>
+
+            <blockquote>
+                And the Lord said unto me, by the Urim and Thummim, <br/>
+                that Kolob was after the manner of the Lord, according to its <br/>
+                times and seasons in the revolutions thereof; that one revolu- <br/>
+                tion was a day unto the Lord, after his manner of reckoning, <br/>
+                it being one thousand years according to the time appointed <br/>
+                unto that whereon thou standest. This is the reckoning of the <br/>
+                Lord's time, according to the reckoning of Kolob. <br/>
+                Abraham 3:4
+            </blockquote>
+
+        </div>
+
+    );
+}
