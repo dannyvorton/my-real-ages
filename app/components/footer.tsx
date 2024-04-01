49af5167f1583db1fa53b3d4cf0c6a7e3edd2f03
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2024-04-01 11:15:05 -0600

    add footer and scripture components

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,13 +3,12 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
 
-  const currentYear = new Date().getFullYear();
-
   return (
 
     <footer className="border-t border-black p-5 text-center text-base">
 
-      <p>Danny Orton, &copy;{currentYear}</p>
+      <p>My Real Ages, is not affiliated with any church or religious organization.</p>
+      <p>Danny Orton</p>
 
       <div className="flex space-x-1 justify-center p-1">
         <Link href="https://github.com/dannyvorton/my-real-ages" target="_blank"><FaGithub className="size-5"/></Link>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Header from "./components/header";
+import Scripture from "./components/scripture";
 import Footer from "./components/footer";
 
 export default function Home() {
   return (
     <>
       <Header/>
+      <Scripture/>
       <Footer/>
     </>
   );
