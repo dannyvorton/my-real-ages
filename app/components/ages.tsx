479a0388b64fc4528e3a1323c712aa880fb0d06a
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2024-04-06 19:58:30 -0600

    add debugger try to fix date

--- app/components/ages.tsx
+++ app/components/ages.tsx
@@ -1,12 +1,15 @@
+import formatDate from "./date";
+
 export default function Ages() {
-
-
 
   return (
 
-    <h3 className="text-3xl text-center p-3">How Old Am I?</h3>
-
-
+    <>
+      <h3 className="text-3xl text-center p-3">How Old Am I?</h3>
+      <h1>currentDate</h1>
+      <div className="text-xl text-center">Enter Birth Date: <input type="date"/></div>
+      <div className="text-xl text-center">Enter Current Date or Death Date: <input type="date"/></div>
+    </>
 
   );
 }
---
+++ app/components/date.tsx
@@ -0,0 +1,13 @@
+export default function formatDate(date = new Date()) {
+
+    const year = date.toLocaleString('default', {year: 'numeric'});
+    const month = date.toLocaleString('default', {month: '2-digit'});
+    const day = date.toLocaleString('default', {day: '2-digit'});
+    let currentDate = [year, month, day].join('-');
+
+    return (
+
+        <p>{ currentDate }</p>
+
+    );
+}
