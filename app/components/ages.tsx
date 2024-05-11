17de7a1a9e3956bbdb7fff04f637453e72346ffc
Dan Orton <danny@Dannys-MacBook-Pro.local>
2024-05-10 21:44:48 -0600

    auto populate date

--- app/components/ages.tsx
+++ app/components/ages.tsx
@@ -1,19 +1,32 @@
 "use client"
 
 import { useState } from "react";
-import currentDate from "./currentDate";
 
 export default function Ages() {
 
-    let [birth, death]: any = useState(currentDate);
+  let date = new Date();
+  let year: string = date.toLocaleString('default', {year: 'numeric'});
+  let month: string = date.toLocaleString('default', {month: '2-digit'});
+  let day: string = date.toLocaleString('default', {day: '2-digit'});
+  let today: string = [year, month, day].join('-');
+
+  let [birth, setBirth]: any = useState(today);
+  let [death, setDeath]: any = useState(today);
 
   return (
 
     <>
       <h3 className="text-3xl text-center p-3">How Old Am I?</h3>
-      <h3></h3>
-      <div className="text-xl text-center">Enter Birth Date: <input type="date" value={birth}/></div>
-      <div className="text-xl text-center">Enter Current Date or Death Date: <input type="date" value={death}/></div>
+
+      <div className="grid grid-cols-2 gap-2">
+        <div className="text-xl text-right">Enter Birth Date:</div>
+        <div className="text-xl text-left"><input type="date" defaultValue={birth}/></div>
+      </div>
+
+      <div className="grid grid-cols-2 gap-2">
+        <div className="text-xl text-right">Enter Current Date or Death Date:</div>
+        <div className="text-xl text-left"><input type="date" defaultValue={death}/></div>
+      </div>
     </>
 
   );
