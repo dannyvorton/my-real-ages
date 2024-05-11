bbd2915f9b5ca78c162ea9751f90567d7e146938
Dan Orton <danny@Dannys-MacBook-Pro.local>
2024-05-10 21:53:49 -0600

    add output

--- app/components/ages.tsx
+++ app/components/ages.tsx
@@ -19,13 +19,25 @@ export default function Ages() {
       <h3 className="text-3xl text-center p-3">How Old Am I?</h3>
 
       <div className="grid grid-cols-2 gap-2">
-        <div className="text-xl text-right">Enter Birth Date:</div>
+        <div className="text-xl text-right">Birth Date:</div>
         <div className="text-xl text-left"><input type="date" defaultValue={birth}/></div>
       </div>
 
       <div className="grid grid-cols-2 gap-2">
-        <div className="text-xl text-right">Enter Current Date or Death Date:</div>
+        <div className="text-xl text-right">Current or Death Date:</div>
         <div className="text-xl text-left"><input type="date" defaultValue={death}/></div>
+      </div>
+
+      <h3 className="text-3xl text-center p-3">My Real Ages Are:</h3>
+
+      <div className="grid grid-cols-2 gap-2">
+        <div className="text-xl text-right">Earth Age:</div>
+        <div className="text-xl text-left">yrs, mos, dys</div>
+      </div>
+
+      <div className="grid grid-cols-2 gap-2">
+        <div className="text-xl text-right">Heaven Age:</div>
+        <div className="text-xl text-left">hrs, min, sec</div>
       </div>
     </>
 
