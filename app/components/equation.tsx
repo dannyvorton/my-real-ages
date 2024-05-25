bab2e8ab13062ca7300f33e36f13e6dfec5c9e3d
Dan Orton <danny@Dannys-MacBook-Pro.local>
2024-05-24 23:48:53 -0600

    update stuff

--- app/components/equation.tsx
+++ app/components/equation.tsx
@@ -4,20 +4,13 @@ export default function Equation() {
   const earthMonth: number = 1;
   const earthYear: number = 1;
 
-  const heavenDay: number = (60 * 60 * 24) * .001;
-  const heavenDaySeconds: number = (heavenDay % 60);
-  const heavenDayMinutes: number = Math.floor((heavenDay - heavenDaySeconds) / 60);
-  const heavenDayHours: number = Math.floor((heavenDay - heavenDaySeconds - heavenDayMinutes) / 24);
+  const heavenDaySeconds: number = ((60 * 60 * 24) * .001);
+  const heavenMonthSeconds: number = heavenDaySeconds * 30.4166667;
+  const heavenYearSeconds: number = heavenDaySeconds * 365;
 
-  const heavenMonth: number = heavenDay * 30.4167;
-  const heavenMonthSeconds: number = (heavenMonth % 60);
-  const heavenMonthMinutes: number = Math.floor((heavenMonth / 60) * .001);
-  const heavenDayMonth: number = Math.floor(heavenMonth / 24);
-
-  const heavenYear: number = heavenDay * 365;
-  const heavenYearSeconds: number = (heavenYear % 60);
-  const heavenYearMinutes: number = Math.floor(heavenYear / 60);
-  const heavenYearMonth: number = Math.floor(heavenYear / 24);
+  console.log(heavenDaySeconds);
+  console.log(heavenMonthSeconds);
+  console.log(heavenYearSeconds);
 
   return (
 
@@ -26,9 +19,9 @@ export default function Equation() {
     <h3 className="text-3xl text-center p-3">Time Conversion</h3>
 
     <div className="grid grid-rows-2 gap-1">
-      <output className="text-xl text-center">{earthDay} day on earth = {heavenDayHours} hours, {heavenDayMinutes} minute, {Math.round(heavenDaySeconds)} seconds in heaven.</output>
-      <output className="text-xl text-center">{earthMonth} month on earth = {heavenDayMonth} hours, {heavenMonthMinutes} minutes, {Math.round(heavenMonthSeconds)} seconds in heaven. </output>
-      <output className="text-xl text-center">{earthYear} year on earth = {heavenYearMonth} hours, {heavenYearMinutes} minutes, {Math.round(heavenYearSeconds)} seconds in heaven.</output>
+      <output className="text-xl text-center">{earthDay} day on earth = {} hours, {} minute, {} seconds in heaven.</output>
+      <output className="text-xl text-center">{earthMonth} month on earth = {} hours, {} minutes, {} seconds in heaven. </output>
+      <output className="text-xl text-center">{earthYear} year on earth = {} hours, {} minutes, {} seconds in heaven.</output>
     </div>
     
     </>
