0e2d57f417615cfbca01fdfe312bae49820f2d58
Danny Orton <52336508+dannyvorton@users.noreply.github.com>
2024-10-13 00:36:31 -0600

    add scriptures

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { ScriptureComponent } from './scripture/scripture.component';
 import { DateComponent } from './date/date.component';
 import { AgeComponent } from './age/age.component';
 import { FooterComponent } from './footer/footer.component';
@@ -8,7 +9,14 @@ import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, DateComponent, AgeComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    ScriptureComponent,
+    HeaderComponent,
+    DateComponent,
+    AgeComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
