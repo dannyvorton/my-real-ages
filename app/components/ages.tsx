87d14dccb39fa5ea48e1a9c66d23130567a4af62
Danny Orton <52336508+dannyvorton@users.noreply.github.com>
2024-10-20 18:59:48 -0600

    update dates

--- app/components/ages.tsx
+++ app/components/ages.tsx
@@ -4,11 +4,11 @@ export default function Ages() {
       <h2 className="text-2xl text-center">My Real Ages Are</h2>
       <div className="grid grid-cols-2 gap-4 p-2">
         <p className="text-right">My earth age is:</p>
-        <p className="text-left">Years, Months, Days</p>
+        <p className="text-left">years, months, days</p>
       </div>
       <div className="grid grid-cols-2 gap-4">
         <p className="text-right">My heaven age is:</p>
-        <p className="text-left">Hours, Minutes, Seconds</p>
+        <p className="text-left">hours, minutes, seconds</p>
       </div>
     </div>
   );
--- app/components/dates.tsx
+++ app/components/dates.tsx
@@ -1,14 +1,27 @@
+"use client"
+
+import { useState } from "react";
+
 export default function Dates() {
+  
+  const current: any = new Date();
+  const year: any = current.getFullYear();
+  const month: any = String(current.getMonth() + 1).padStart(2, "0");
+  const day: any = current.getDate();
+  const today: any = year + "-" + month + "-" + day;
+  const [now, getNow] = useState(today);
+  const handleChange = () => {getNow};
+
   return (
     <div className="p-4">
-      <h2 className="text-2xl text-center">Enter Dates For Real Ages</h2>
+      <h2 className="text-2xl text-center">Enter Dates For Your Real Ages</h2>
       <div className="grid grid-cols-2 gap-4 p-2">
         <p className="text-right">Enter birth date:</p>
-        <span><input type="date"/></span>
+        <span><input type="date" value={now} onChange={handleChange}/></span>
       </div>
       <div className="grid grid-cols-2 gap-4">
         <p className="text-right">Enter death or current date:</p>
-        <span><input type="date"/></span>
+        <span><input type="date" value={now} onChange={handleChange}/></span>
       </div>
     </div>
   );
