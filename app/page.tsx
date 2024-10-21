44481cd48fb08b4a3d2e6016055e866fe4d982ca
Robert Bischoff <robert_bischoff@me.com>
2024-10-21 12:45:38 -1000

    Date and Age Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,16 @@
-import Image from "next/image";
 import Head from "./components/head";
 import Scripture from "./components/scripture";
 import Dates from "./components/dates";
-import Ages from "./components/ages";
+//import Ages from "./components/ages";
 import Foot from "./components/foot";
 
 export default function Home() {
   return (
     <>
-      <Head/>
-      <Scripture/>
-      <Dates/>
-      <Ages/>
-      <Foot/>
+      <Head />
+      <Scripture />
+      <Dates />
+      <Foot />
     </>
   );
 }
