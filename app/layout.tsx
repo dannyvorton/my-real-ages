79f2edb104d6f0931e7bad66ec329fdc797b143c
dannyvorton <dan.orton@live.com>
2025-12-26 16:49:28 -0700

    add className

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body class={ inter.className }>
+      <body className={ inter.className }>
         {children}
       </body>
     </html>
