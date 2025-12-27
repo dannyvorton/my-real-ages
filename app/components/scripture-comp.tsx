1663b151c1a223075deb2a767720804832093a68
dannyvorton <dan.orton@live.com>
2025-12-26 17:01:16 -0700

    remove tailwindcss

--- app/components/scripture-comp.tsx
+++ app/components/scripture-comp.tsx
@@ -4,7 +4,7 @@ export default function ScriptureComp() {
   return (
     <div>
       <article>
-        For a thousand years in thy sight are but as yesterday when <br className="hidden sm:inline"/>
+        For a thousand years in thy sight are but as yesterday when <br/>
         it is past, and as a watch in the night. <br/>
         <Link
           href="https://www.churchofjesuschrist.org/study/scriptures/ot/ps/90?lang=eng"
@@ -12,8 +12,8 @@ export default function ScriptureComp() {
         >Psalm 90:4</Link>
       </article>
       <article>
-        But, beloved, be not ignorant of this one thing, that one day <br className="hidden sm:inline"/>
-        is with the Lord as a thousand years, and a thousand years as <br className="hidden sm:inline"/>
+        But, beloved, be not ignorant of this one thing, that one day <br/>
+        is with the Lord as a thousand years, and a thousand years as <br/>
         one day. <br/>
         <Link
           href="https://www.churchofjesuschrist.org/study/scriptures/nt/2-pet/3?lang=eng"
@@ -21,13 +21,13 @@ export default function ScriptureComp() {
         >2 Peter 3:8</Link>
       </article>
       <article>
-        And the Lord said unto me, by the Urim and Thummim, <br className="hidden sm:inline"/>
-        that Kolob was after the manner of the Lord, according to its <br className="hidden sm:inline"/>
-        times and seasons in the revolutions thereof; that one <br className="hidden sm:inline"/>
-        revolution was a day unto the Lord, after his manner of <br className="hidden sm:inline"/>
-        reckoning, it being one thousand years according to the time <br className="hidden sm:inline"/>
-        appointed unto that whereon thou standest. This is the <br className="hidden sm:inline"/>
-        reckoning of the Lord&#39;s time, according to the reckoning of <br className="hidden sm:inline"/>
+        And the Lord said unto me, by the Urim and Thummim, <br/>
+        that Kolob was after the manner of the Lord, according to its <br/>
+        times and seasons in the revolutions thereof; that one <br/>
+        revolution was a day unto the Lord, after his manner of <br/>
+        reckoning, it being one thousand years according to the time <br/>
+        appointed unto that whereon thou standest. This is the <br/>
+        reckoning of the Lord&#39;s time, according to the reckoning of <br/>
         Kolob. <br/>
         <Link
           href="https://www.churchofjesuschrist.org/study/scriptures/pgp/abr/3?lang=eng"
