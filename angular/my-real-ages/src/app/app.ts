949e89eccbe626bd681bca1dc6d79739100a097c
dannyvorton <dan.orton@live.com>
2026-01-12 21:28:09 -0700

    work on components

--- angular/my-real-ages/src/app/app.ts
+++ angular/my-real-ages/src/app/app.ts
@@ -1,9 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Header } from './header/header';
+import { Scriptures } from './scriptures/scriptures';
+import { Dates } from './dates/dates';
+import { Ages } from './ages/ages';
+import { Footer } from './footer/footer';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Header,
+    Scriptures,
+    Dates,
+    Ages,
+    Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
